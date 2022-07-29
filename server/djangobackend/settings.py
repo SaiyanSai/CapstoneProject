1a91a0e8491ea37e305e36324f1394f3c5262ac6
SaiyanSai <sents@farmingdale.edu>
2022-07-29 10:18:40 +0000

    ready for cicd

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "SaiyanSaiCapstone.us-south.cf.appdomain.cloud"
+
+]
 
 
 # Application definition
